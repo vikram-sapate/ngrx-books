33bef19dba3b9b825ba2b23f96362eb163cf8255
vikram-sapate <vikram.sapate123@gmail.com>
2022-07-14 16:25:30 +0530

    2 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { booksReducer } from './state/books.reducer';
 import { collectionReducer } from './state/collection.reducer';
 import { BookCollectionComponent } from './book-collection/book-collection.component';
 import { BookListComponent } from './book-list/book-list.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports: [
     BrowserModule,
+    HttpClientModule,
     StoreModule.forRoot({ books: booksReducer, collection: collectionReducer }),
   ],
   declarations: [AppComponent, BookListComponent, BookCollectionComponent],
